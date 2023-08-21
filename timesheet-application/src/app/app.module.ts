55316c53e6d84f474313cf8b78968a8ca2ca9fd7
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-08-20 20:12:43 -0600

    Created Basic Components

--- timesheet-application/src/app/app.module.ts
+++ timesheet-application/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    AnalyticsComponent,
+    AnalyticsTableComponent,
+    TopNavbarComponent,
+    TimesheetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
