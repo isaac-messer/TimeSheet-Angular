cc904271994ce2665f5ada8159acb44c5f2073dd
Isaac Messer <101677325+isaac-messer@users.noreply.github.com>
2023-08-22 11:13:06 -0600

    Added Service to Populate Departments.html List

--- timesheet-application/src/app/components/departments/departments.component.ts
+++ timesheet-application/src/app/components/departments/departments.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { Department } from 'src/app/interfaces/department';
 
 @Component({
   selector: 'app-departments',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent {
+  departments: Department [];
+  
+  constructor(
+    private departmentsService: DepartmentsService,
+  ) {}
 
-}
+    ngOnInit(): void{
+      this.departments = this.departmentsService.departments;
+    };
+};
